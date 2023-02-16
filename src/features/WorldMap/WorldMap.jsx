5af0528b8d71450bc859bb55c6d2342bed7e2d34
SsbThatsMe <samuel.broms@gmail.com>
2023-02-16 12:36:43 +0100

    Removed some code I didn't end up using.
    Accidentally forgot to remove some lines earlier so I did some cleanup now!

--- src/features/WorldMap/WorldMap.jsx
+++ src/features/WorldMap/WorldMap.jsx
@@ -17,12 +17,8 @@ export const setSelected = (id) => {
 // Adapted from:
 // https://www.pluralsight.com/guides/using-d3.js-inside-a-react-app
 
-var body = document.body,
-    html = document.documentElement;
-
 const canvasWidth = "100%";
-const canvasHeight = "100%";  //Math.max( body.scrollHeight, body.offsetHeight, 
-                       //html.clientHeight, html.scrollHeight, html.offsetHeight );
+const canvasHeight = "100%";
 const categories = ['Omission --> Commission', 'Passengers --> Pedestrians', 'Law: Illegal --> Legal', 'Gender: Male --> Female', 'Fitness: Large --> Fit', 'Social Status: Low --> High', 'Age: Elderly --> Young', 'Number of Characters: Less --> More', 'Species: Pets --> Humans' ]
 
 export default function WorldMap() {
