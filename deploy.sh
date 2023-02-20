7d6448c1da439f5dd5c047db0c0a454303572fac
Sebastian Willenbrink <sebastian.willenbrink@tum.de>
2023-02-20 16:43:44 +0100

    Experiment with actions

--- deploy.sh
+++ deploy.sh
@@ -13,28 +13,28 @@ set -e
 npm install
 npm run build
 
-cd dist
+# cd dist
 
 # bypass Jekyll processing
-echo > .nojekyll
+echo > dist/.nojekyll
 
 
 # Setup git on the build VM
-git config --global user.name ${GITHUB_ACTOR}
-git config --global user.email "<>"
+# git config --global user.name ${GITHUB_ACTOR}
+# git config --global user.email "<>"
 
 # Provide a helpful timestamp to commits 
 # this initiates a sub-git-repo (uggly work-around) to make sure we only get the dist files in the commit to github-pages
-git init
+# git init
 # create a branch in sub-git-repo
-git checkout -B main
+# git checkout -B main
 # do the commit (only contains dist)
-git add -A
-git commit -m "[Automated] GitHub Pages deploy script
-[$(date '+%F@%T (%Z)')]"
+# git add -A
+# git commit -m "[Automated] GitHub Pages deploy script
+# [$(date '+%F@%T (%Z)')]"
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # push the contents of the main (branch in sub-git-repo) to gh-pages branch. We need to specify the git@github address because in the sub-repo we don't have this as origin
-git push -f https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/Willenbrink/Ivis-project.git main:gh-pages
+# git push -f https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/Willenbrink/Ivis-project.git main:gh-pages
 
-cd -
+# cd -
