9275ee86684789dc03da1869fe62d6af549fd41d
Sebastian Willenbrink <sebastian.willenbrink@tum.de>
2023-02-24 21:51:54 +0100

    Remove interiorBorders, use edge of countries

--- src/features/WorldMap/parseMapJSON.jsx
+++ src/features/WorldMap/parseMapJSON.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useCallback, useEffect } from "react";
 import { json } from "d3";
-import { feature, mesh } from "topojson-client";
+import { feature } from "topojson-client";
 import { get_country_data } from "../../model/dataHandler";
 import iso from "iso-3166-1";
 
@@ -41,7 +41,6 @@ export const parseJSON = () => {
       setData({
         iso_countries,
         non_iso_countries,
-        interiorBorders: mesh(jsonData, jsonData.objects.countries),
       });
     });
   }, []);
