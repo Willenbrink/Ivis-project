25f1b0e1026c4049f92f632bc180d6d2909825fe
vlle1 <36682087+vlle1@users.noreply.github.com>
2023-03-13 22:08:13 +0100

    Update Icons for Intervention

--- src/assets/legendImages/intervention/InterventionFrom.jsx
+++ src/assets/legendImages/intervention/InterventionFrom.jsx
@@ -1,7 +1,22 @@
-export default function InterventionFrom(){
-  return(
-    <svg width="100%" height="100%" viewBox="0 0 16 56" fill="none" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid meet">
-    <path d="M11.1364 1H4.86364V47.4351H2L8 55L14 47.4351H11.1364V1Z" fill="#D9FFCC" stroke="#3B3B3B"/>
+export default function InterventionFrom() {
+  return (
+    <svg
+      width="100%"
+      height="100%"
+      viewBox="0 0 410 250"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      preserveAspectRatio="xMaxYMid meet"
+    >
+      <path d="M0 267V0H256V267H0Z" fill="#999999" />
+      <path d="M125 262H131.5V215H125V262Z" fill="white" />
+      <path d="M125 193H131.5V146H125V193Z" fill="white" />
+      <path d="M125 123H131.5V76H125V123Z" fill="white" />
+      <path d="M125 54H131.5V7H125V54Z" fill="white" />
+      <path
+        d="M75.5 24H53L52.5 222H42L64 248.5L86 222H75.5V24Z"
+        fill="#D9FFCC"
+      />
     </svg>
-  )
+  );
 }
--- src/assets/legendImages/intervention/InterventionTo.jsx
+++ src/assets/legendImages/intervention/InterventionTo.jsx
@@ -1,7 +1,22 @@
-export default function InterventionTo(){
-  return(
-     <svg width="100%" height="100%" viewBox="0 0 40 56" fill="none" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid meet">
-     <path d="M7.43367 1H1.00613L1 3.42857L1.57747 7.14286L3.00581 13.4286L5.14832 20.4286L7.43367 25.1429L10.4332 29L14.004 33.7143L31.4298 52L29.4301 53.7143L39 55L38.4287 45.4286L36.2861 47.1429L33.2866 44.1429L23.8596 33.7143L19.1461 28.4286L14.004 21.7143L11.2902 16L9.43334 10.2857L8.005 5.28571L7.43367 1Z" fill="#D9FFCC" stroke="#3B3B3B"/>
-     </svg>
-  )
+export default function InterventionTo() {
+  return (
+    <svg
+      width="100%"
+      height="100%"
+      viewBox="0 0 580 250"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      preserveAspectRatio="xMaxYMid meet"
+    >
+      <path d="M0 267V0H256V267H0Z" fill="#999999" />
+      <path d="M125 262H131.5V215H125V262Z" fill="white" />
+      <path d="M125 193H131.5V146H125V193Z" fill="white" />
+      <path d="M125 123H131.5V76H125V123Z" fill="white" />
+      <path d="M125 54H131.5V7H125V54Z" fill="white" />
+      <path
+        d="M80.5215 24H58.0215L58 32.5L60.0215 45.5L65.0215 67.5L72.5215 92L80.5215 108.5L91.0215 122L103.521 138.5L164.521 202.5L157.521 208.5L191.021 213L189.021 179.5L181.521 185.5L171.021 175L138.021 138.5L121.521 120L103.521 96.5L94.0215 76.5L87.5215 56.5L82.5215 39L80.5215 24Z"
+        fill="#D9FFCC"
+      />
+    </svg>
+  );
 }
