c81d3a80702ad91666fbe8113db5d7c5ddfb4ff9
Marcus Gåhlin <marcus.gahlin94@gmail.com>
2023-03-21 09:08:40 +0100

    learning objectives

--- src/utils/teamMembersInfo.js
+++ src/utils/teamMembersInfo.js
@@ -3,30 +3,35 @@ export const teamMembersInfo = [
     name: 'Marcus Gåhlin',
     email: 'mgah@kth.se',
     linkedin: '',
-    work: 'React, D3, Frontend and prototyping'
+    work: 'React, D3, Frontend and sketching',
+    learning: 'Learning about D3, React and the visualization pipeline for maps.'
   },
   {
     name: 'Sebastian Willenbrink',
     email: 'stwi@kth.se',
     linkedin: '',
-    work: 'Preprocessing and React'
+    work: 'Preprocessing and React',
+    learning: ''
   },
   {
     name: 'Samuel Sandberg Bröms',
     email: 'samuelsb@kth.se',
     linkedin: '',
-    work: 'D3 and Front-end'
+    work: 'D3 and Front-end',
+    learning: 'Learning about D3, React, and front-end design.'
   },
   {
     name: 'Valentin Quapil',
     email: 'quapil@kth.se',
     linkedin: '',
-    work: 'React and D3'
+    work: 'React and D3',
+    learning: ''
   },
   {
     name: 'Alex Modee',
     email: 'amodee@kth.se',
     linkedin: '',
-    work: 'User evaluation and sketching'
+    work: 'User evaluation and sketching',
+    learning: 'Learning about the visualisation pipeline, color mapping of data and designing and creating interactive visualizations.'
   }
 ]
